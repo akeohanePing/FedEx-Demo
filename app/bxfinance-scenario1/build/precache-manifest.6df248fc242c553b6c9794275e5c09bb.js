426f6ae7430650d9d37fdfc5f2bfb2fa1fea0c75
akeohanePing <andrewkeohane@pingidentity.com>
2021-03-22 12:54:38 -0600

    add front end app

---
+++ app/bxfinance-scenario1/build/precache-manifest.6df248fc242c553b6c9794275e5c09bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88de56680eda1e42eb89b1b3bfef9e11",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "f449d5f9a01d9e70e9b2",
+    "url": "/app/static/css/main.9dae4c55.chunk.css"
+  },
+  {
+    "revision": "d944bf9c1294a76bd201",
+    "url": "/app/static/js/2.89ada182.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/app/static/js/2.89ada182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f449d5f9a01d9e70e9b2",
+    "url": "/app/static/js/main.53904f3b.chunk.js"
+  },
+  {
+    "revision": "9a5319d8fa863abc5197",
+    "url": "/app/static/js/runtime-main.eab47560.js"
+  },
+  {
+    "revision": "f9f3a4bf508eec8270bf7c8fe4397384",
+    "url": "/app/static/media/fedExLogo.f9f3a4bf.png"
+  },
+  {
+    "revision": "0be730ab075542647aa55a5b700c4449",
+    "url": "/app/static/media/heroImage.0be730ab.jpeg"
+  }
+]);
