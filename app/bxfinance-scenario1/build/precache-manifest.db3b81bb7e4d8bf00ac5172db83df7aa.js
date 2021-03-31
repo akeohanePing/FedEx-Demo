e77a378fe73615f5315a577306fee243072717e0
Ubuntu <azureuser@Fedex-POC.gbmm4w2h0vmejahlqsyjrupkqb.bx.internal.cloudapp.net>
2021-03-31 20:24:39 +0000

    updating env_vars

---
+++ app/bxfinance-scenario1/build/precache-manifest.db3b81bb7e4d8bf00ac5172db83df7aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd50b99a5af20aa1b0e6585ff6ea79f",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "372eddc7811a4e9bdc65",
+    "url": "/app/static/css/main.3dcfedf4.chunk.css"
+  },
+  {
+    "revision": "e79ff57f17a3d5ed2637",
+    "url": "/app/static/js/2.fe29b734.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/app/static/js/2.fe29b734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372eddc7811a4e9bdc65",
+    "url": "/app/static/js/main.0ed8da1d.chunk.js"
+  },
+  {
+    "revision": "9a5319d8fa863abc5197",
+    "url": "/app/static/js/runtime-main.eab47560.js"
+  },
+  {
+    "revision": "f9f3a4bf508eec8270bf7c8fe4397384",
+    "url": "/app/static/media/fedExLogo.f9f3a4bf.png"
+  },
+  {
+    "revision": "0be730ab075542647aa55a5b700c4449",
+    "url": "/app/static/media/heroImage.0be730ab.jpeg"
+  }
+]);
